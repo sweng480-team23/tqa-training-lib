c8997eed63302808a113052cf93e2bc67f32ae3c
Luke Westfall <lwestfall1@gmail.com>
2022-02-01 08:02:26 -0500

    make trainingargs json serializable

--- train.py
+++ train.py
@@ -3,11 +3,10 @@ from tqa_training_lib.data_preparation_lib import prepare_data
 from tqa_training_lib.model_scoring_lib import score_model
 from tqa_training_lib.model_training_lib import do_train
 from transformers import TrainingArguments
-import json
+import jsonpickle
 
 model_out_path = 'model_out/'
 log_out_path = model_out_path + 'logs'
-
 
 df = extract_data('https://raw.githubusercontent.com/sweng480-team23/tweet-qa-data/main/train.json')
 train_encodings, val_encodings = prepare_data(df)
@@ -27,9 +26,9 @@ training_args = TrainingArguments(
     save_steps=500
 )
 
-# training args not json serializable
-# can either pickle, or choose which fields to serialize
-# json.dump(training_args, 'model_out/args.json')
+f = open('model_out/args.json', 'w')
+json_obj = jsonpickle.encode(training_args)
+f.write(json_obj)
 
 do_train(
     train_encodings,
