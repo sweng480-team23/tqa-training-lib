7e6b0d14c5413830da02ad31b37a1a796f94dc42
Luke Westfall <lwestfall1@gmail.com>
2022-03-07 18:27:48 -0500

    cleanup

--- score.py
+++
@@ -1,4 +0,0 @@
-from tqa_training_lib.model_scoring_lib import score_model
-
-
-score_model('model_out/', save_gold_user_files=True, print_scores=True, use_tf=True)
--- try_prep.py
+++
@@ -1,5 +0,0 @@
-from tqa_training_lib.data_extraction_lib import extract_data
-from tqa_training_lib.data_preparation_lib import prepare_data
-
-df = extract_data('https://raw.githubusercontent.com/sweng480-team23/tweet-qa-data/main/train.json')
-train_encodings, val_encodings = prepare_data(df, save_data=True, print_stats=True)
