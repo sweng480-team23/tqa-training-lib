a64d22d3975ffc3b7bf13e53df133b665c283aa2
Luke Westfall <lwestfall1@gmail.com>
2022-03-10 07:38:04 -0500

    Remove install requirements

--- setup.py
+++ setup.py
@@ -4,13 +4,6 @@ import setuptools
 with open("readme.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-requirements_path = os.path.dirname(os.path.realpath(__file__)) + '/requirements.txt'
-
-install_requires = []
-if os.path.isfile(requirements_path):
-    with open(requirements_path) as f:
-        install_requires = f.read().splitlines()
-
 setuptools.setup(
     name="tqa_training_lib",
     version="0.0.2",
@@ -21,7 +14,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/sweng480-team23/tqa-training-lib",
     packages=setuptools.find_packages(),
-    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
