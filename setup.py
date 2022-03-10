60424d9d37b17c9304fb426b9e7c415c73fdb9d7
Luke Westfall <lwestfall1@gmail.com>
2022-03-09 19:28:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open("readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
-    name="tqa-training-lib",
-    version="0.0.1",
+    name="tqa_training_lib",
+    version="0.0.2",
     author="Luke Westfall et al",
     author_email="lwestfall1@gmail.com",
     description="Common functions for tweetqa model training",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/sweng480-team23/tqa-training-lib",
     packages=setuptools.find_packages(),
     classifiers=[
