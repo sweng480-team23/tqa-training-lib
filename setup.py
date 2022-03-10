e1f05e16c6a9ffc5197746f2b5289db3b19284bc
Luke Westfall <lwestfall1@gmail.com>
2022-03-09 19:32:13 -0500

    Oops

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/sweng480-team23/tqa-training-lib",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
